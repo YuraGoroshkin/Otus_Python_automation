c41f5870ad8e7f91607d9dc1e51c0cfdc855889b
Yura <gyrra777@gmail.com>
2023-05-28 10:51:18 +0300

    params for dog api

--- tests/test_dog_api.py
+++ tests/test_dog_api.py
@@ -1,18 +1,19 @@
-import re
-
 import pytest
 import requests
 from files.schema_dog_list_all import schema
 from jsonschema import validate
-import random
 
 url = "https://dog.ceo/"
 
 
-def test_status_code():
+@pytest.mark.parametrize('status', [200, 300, 400, 500])
+def test_status_code(status):
     path = 'dog-api/'
     response = requests.get(url + path)
-    assert response.status_code == 200
+    if status == 200:
+        assert response.status_code == 200
+    else:
+        assert response.status_code != status
 
 
 def test_json_schema():
@@ -27,11 +28,15 @@ def test_header_json():
     assert response.headers["Content-Type"] == "application/json"
 
 
-def test_num_fotos():
-    num = random.randint(0, 30)
+# запрос не может вернуть более 50 объектов по документации
+@pytest.mark.parametrize('num', [3, 5, 10, 49, 50, 51, 100])
+def test_num_fotos(num):
     path = "api/breeds/image/random/" + str(num)
     response = requests.get(url + path)
-    assert len(response.json()["message"]) == num
+    if num > len(response.json()["message"]):
+        assert len(response.json()["message"]) != num
+    else:
+        assert len(response.json()["message"]) == num
 
 
 def test_jpg_foto():
